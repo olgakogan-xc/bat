042d8abc184071074f5e8fce00efb5f5a8dc0160
Olga Kogan <Olga.Kogan@xcentium.com>
2017-05-04 14:55:39 -0700

    Changged the default action method

--- AssetmarkBAT/App_Start/RouteConfig.cs
+++ AssetmarkBAT/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AssetmarkBAT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AssetmarkBAT", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
